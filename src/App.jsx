f743d7d990f4e5fad82e33b3085e1509b2f5e203
Pablo Rodriguez <pa.rodriguezg@gmail.com>
2024-11-28 15:40:40 -0300

    V 1.2 Incorpora un nuevo componente llamado Cart.jsx que contiene el carrito de compras, Reemplaza el origen de datos por un archivo JSON llamado pizzas.js que luego se renderiza dinamicamente en los componentes CardPizza.jsx y Cart.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,19 @@ import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import Register from "./pages/components/Register";
 import Login from "./pages/components/Login";
+import Cart from "./pages/components/Cart";
 
 function App() {
   return (
     <>
-      <Navbar />
-      {/*<Home />*/}
-      <Register />
-      {/* <Login /> */}
-      <Footer />
+      <div className="flex flex-col min-h-screen">
+        <Navbar />
+        <main className="flex-grow">{/*<Home />*/}</main>
+        {/* <Register /> */}
+        {/* <Login /> */}
+        {<Cart />}
+        <Footer />
+      </div>
     </>
   );
 }
